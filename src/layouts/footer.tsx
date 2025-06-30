63aed07e205394f30b73d23d41268bba3167b9e1
김주희 <kimjuhee@gimjuhuis-MacBook-Air.local>
2025-06-30 14:04:09 +0900

    Feat: Footer 구현#1

---
+++ src/layouts/footer.tsx
@@ -0,0 +1,29 @@
+import LogoSVG from "@/icons/logo-white.svg";
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 w-full bg-white text-gray-900 py-6 px-4 sm:px-6 lg:px-8 border-t border-gray-200">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="flex items-center space-x-3 mb-4 md:mb-0">
+            <LogoSVG width={30} />
+            <div>
+              <h3 className="font-bold text-sm">CERT-IS</h3>
+              <p className="text-xs text-gray-500">
+                부경대학교 사이버 보안 동아리
+              </p>
+            </div>
+          </div>
+          <div className="text-center md:text-right">
+            <p className="text-xs text-gray-500">
+              {/* © 2025 CERT-IS. 하나씩 버그를 잡으며 디지털 공간을 보호합니다. */}
+              © 2007. PKNU CERT-IS.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
