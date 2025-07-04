100c32e366eaf1bad987cdeb317c3ffdefbe8b9c
김주희 <kimjuhee@gimjuhuis-MacBook-Air.local>
2025-07-04 12:46:41 +0900

    Fix: overscroll 추가#1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="overscroll-none">
         <header>
           <NavigationBar />
         </header>
