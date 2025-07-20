8f6d533ec48330c20c008bcb590750c9b4ae0c69
yeomin4242 <whereismysejong@naver.com>
2025-07-20 14:57:09 +0900

    Feat: Impl new blog article modal component#18

---
+++ src/app/blog/@modal/(.)write/page.tsx
@@ -0,0 +1,10 @@
+import NewPageModal from "@/components/write/CCNewPageModal";
+import BlogWritePage from "@/app/blog/write/page";
+
+export default function WriteModalPage() {
+  return (
+    <NewPageModal>
+      <BlogWritePage />
+    </NewPageModal>
+  );
+}
